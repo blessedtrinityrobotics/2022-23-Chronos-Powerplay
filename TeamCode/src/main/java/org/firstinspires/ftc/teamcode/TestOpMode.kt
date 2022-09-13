8ea00c35aad182083076b1dd9518f5a796d05553
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2022-09-13 16:59:54 -0400

    Starting coding :dizzy_face:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.kt
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode
 
-class TestOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp(name="Test TeleOp")
+class TestOpMode : OpMode() {
+
+
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun loop() {
+        TODO("Not yet implemented")
+    }
+
 }
