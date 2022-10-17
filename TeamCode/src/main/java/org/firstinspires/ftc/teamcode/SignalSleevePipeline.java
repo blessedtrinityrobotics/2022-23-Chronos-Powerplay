6f242dfc70414a8cf020e5dec94ade8e1e46dfc9
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2022-10-17 16:36:48 -0400

    added Pics and Pipes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalSleevePipeline.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+/*
+* All colors use HSV
+*
+* Magenta: (307.8, 100, 100)
+* Green: (115.8, 100, 100)
+* Teal: (174.4, 100, 100)
+*
+*
+*/
+
+public class SignalSleevePipeline {
+}
