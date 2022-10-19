5f9730a4a94d0205d49d88e1eddeccf14f71bf9d
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2022-10-19 16:57:22 -0400

    imported Image processing from GRIP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalSleevePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalSleevePipeline.java
@@ -1,14 +1,104 @@
 package org.firstinspires.ftc.teamcode;
 
-/*
-* All colors use HSV
-*
-* Magenta: (307.8, 100, 100)
-* Green: (115.8, 100, 100)
-* Teal: (174.4, 100, 100)
-*
-*
-*/
+import org.opencv.core.*;
+import org.opencv.imgproc.*;
+import org.openftc.easyopencv.OpenCvPipeline;
 
-public class SignalSleevePipeline {
+public class SignalSleevePipeline extends OpenCvPipeline {
+
+    //Outputs
+    private Mat blurOutput = new Mat();
+    private Mat zone3Output = new Mat(); // Blue
+    private Mat zone2Output = new Mat(); // Green
+    private Mat zone1Output = new Mat(); // Pink
+
+    private int zone = 0;
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    /**
+     * This is the primary method that runs the entire pipeline and updates the outputs.
+     */
+    public Mat processFrame(Mat source0) {
+        // Step Blur0:
+        Mat blurInput = source0;
+        double blurRadius = 19.0;
+        blur(blurInput, blurRadius, blurOutput);
+
+        // Step HSV_Threshold0:
+        Mat zone3Input = blurOutput;
+        double[] zone3Hue = {87, 105};
+        double[] zone3Saturation = {94, 255};
+        double[] zone3Value = {87, 255};
+        hsvThreshold(zone3Input, zone3Hue, zone3Saturation, zone3Value, zone3Output);
+
+        // Step HSV_Threshold1:
+        Mat zone2Input = blurOutput;
+        double[] zone2Hue = {56, 85};
+        double[] zone2Saturation = {91, 145};
+        double[] zone2Value = {50, 151};
+        hsvThreshold(zone2Input, zone2Hue, zone2Saturation, zone2Value, zone2Output);
+
+        // Step HSV_Threshold2:
+        Mat zone1Input = blurOutput;
+        double[] zone1Hue = {142, 172};
+        double[] zone1Saturation = {103, 255};
+        double[] zone1Value = {116, 255};
+        hsvThreshold(zone1Input, zone1Hue, zone1Saturation, zone1Value, zone1Output);
+
+        int zone1Count = Core.countNonZero(zone1Output);
+        int zone2Count = Core.countNonZero(zone2Output);
+        int zone3Count = Core.countNonZero(zone3Output);
+
+        if (zone1Count > Math.max(zone2Count, zone1Count)) {
+            zone = 1;
+        } else if (zone2Count > Math.max(zone1Count, zone3Count)) {
+            zone = 2;
+        } else {
+            zone = 3;
+        }
+
+        return source0;
+    }
+
+    public int getZone() {
+        return zone;
+    }
+
+    private void setZone(int z) {
+        zone = z;
+    }
+
+    /**
+     * Softens an image using one of several filters.
+     * @param input The image on which to perform the blur.
+     * @param doubleRadius The radius for the blur.
+     * @param output The image in which to store the output.
+     */
+    private void blur(Mat input, double doubleRadius,
+                      Mat output) {
+        int radius = (int)(doubleRadius + 0.5);
+        int kernelSize = 6 * radius + 1;
+        Imgproc.GaussianBlur(input, output, new Size(kernelSize, kernelSize), radius);
+    }
+
+    /**
+     * Segment an image based on hue, saturation, and value ranges.
+     *
+     * @param input The image on which to perform the HSL threshold.
+     * @param hue The min and max hue
+     * @param sat The min and max saturation
+     * @param val The min and max value
+     */
+    private void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val,
+                              Mat out) {
+        Imgproc.cvtColor(input, out, Imgproc.COLOR_RGB2HSV); // EasyOpenCV uses RGB instead of BGR
+        Core.inRange(out, new Scalar(hue[0], sat[0], val[0]),
+                new Scalar(hue[1], sat[1], val[1]), out);
+    }
+
 }
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/AltMechanumTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/AltMechanumTeleOp.kt
@@ -1,18 +1,18 @@
-﻿package org.firstinspires.ftc.teamcode.teleops
+package org.firstinspires.ftc.teamcode.teleops
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.util.Range
 import org.firstinspires.ftc.teamcode.CRAWL_SPEED
 import org.firstinspires.ftc.teamcode.smoothInput
 
-@TeleOp(name="Mechanum TeleOp")
-class MechanumTeleOp : BaseTeleOp() {
+@TeleOp(name="Alternate Mechanum TeleOp (Jack Likes it)")
+class AltMechanumTeleOp : BaseTeleOp() {
     override fun drive() {
         val boost = gamepad1.right_trigger.toDouble()
         val multiplier = 1.0 - Range.scale(boost, 0.0, 1.0, 0.0, 1.0 - CRAWL_SPEED)
         val throttle = smoothInput(gamepad1.left_stick_y.toDouble()) * multiplier
-        val strafe = smoothInput(gamepad1.left_stick_x.toDouble()) * multiplier * 1.1
-        val turn = smoothInput(gamepad1.right_stick_x.toDouble()) * multiplier
+        val turn = smoothInput(gamepad1.left_stick_x.toDouble()) * multiplier * 1.1
+        val strafe = smoothInput(gamepad1.right_stick_x.toDouble()) * multiplier
 
         drivetrain.holonomicDrive(throttle, strafe, turn)
     }
